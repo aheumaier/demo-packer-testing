9b4a51cb88096ead76df8b8c7bf2dd509ec789ef
Andreas Heumaier <andreas.heumaier@microsoft.com>
2020-11-04 12:42:06 +0100

    fix: remove debug out; add python shebang in main.py

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -exu
 
-PACKAGES=(vim python-pip apt-utils)
+PACKAGES=(python-pip)
 
 prep_docker() {
     apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -yq "${PACKAGES[@]}"
@@ -11,11 +11,7 @@ prep_docker() {
 
 prep_azure() {
     sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq "${PACKAGES[@]}"
-    ls -l /tmp/
-    ls -l /tmp/app/
     sudo mv /tmp/app/ /opt/app/
-    ls -l /opt/
-    ls -l /opt/app/
     pip install -r /opt/app/requirements.txt
 
     sudo mv /opt/app/flask.service /etc/systemd/system/
@@ -32,6 +28,6 @@ run_main() {
     elif [ "${PACKER_BUILDER_TYPE}" == "azure-arm" ]; then
         prep_azure
     fi
-}
+}-
 
 run_main
