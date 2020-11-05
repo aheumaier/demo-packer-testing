cb86aad5bcba928ed5e74e691fcadb4dc322cc82
Dariusz Parys <dparys@microsoft.com>
2020-11-05 13:40:18 +0100

    feat: Adding Windows Testinfra to Packer Build
    In order to have Testinfra work with windows packer builds the
    configuration needs to support local shell execution. This is
    done via the local shell provider from packer. The local shell
    provider supports out of the box cmd shell. If you want to use
    powershell you have to configure execute_command and corresponding
    variables.
    
    It turned out that there is a bug in the provisioner when specifying
    a powershell script through the local shell provider. It spawns a
    mouse settings dialog when executed and doesn't continue. Turning
    to inline scripts worked fine.
    
    https://www.packer.io/docs/provisioners/shell-local.html
    
    Further the configuration needs to support WinRM calls via basic
    authentication, which is enabled in the setup.ps1 script.
    
    https://testinfra.readthedocs.io/en/latest/backends.html#winrm
    
    Testinfra's WinRM leverages the pywinrm package
    
    https://github.com/diyan/pywinrm
    
    This commit has been tested successfully on Windows as the packer
    host, also the script had to be split as we have a dedicated set
    of tests for the Windows image.

---
+++ setup.ps1
@@ -0,0 +1,36 @@
+$VerbosePreference="Continue"
+
+function ConfigureWinRM {
+    Write-Output "INFO: setup.ps1: ConfigureWinRM"
+
+    winrm set winrm/config/client '@{TrustedHosts="*"}'
+    winrm quickconfig -q
+    netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol="icmpv4:8,any" dir=in action=allow
+    New-NetFirewallRule -DisplayName 'WinRM' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 5985
+}
+
+function InstallChocolatey {
+    Write-Output "INFO: setup.ps1: InstallChocolatey"
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    
+    choco feature enable --name allowGlobalConfirmation
+    choco feature disable --name showDownloadProgress
+}
+
+function InstallPackages {
+    Write-Output "INFO: setup.ps1: InstallPackages"
+    choco install python
+    choco install vim
+}
+
+function Main {
+    Write-Output "INFO: setup.ps1: Main"
+    InstallChocolatey
+    InstallPackages
+    ConfigureWinRM
+}
+
+Main
