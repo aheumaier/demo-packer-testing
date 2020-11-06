c218b66fa244b941c3b867e614c8f36332457cb9
Andreas Heumaier <andreas.heumaier@microsoft.com>
2020-11-06 09:53:05 +0100

    Add test method skipif based on OS

--- test_build_integration.py
+++ test_build_integration.py
@@ -1,8 +1,10 @@
 # more info:
 # https://testinfra.readthedocs.io/en/latest
 import pytest
+import platform
 
 
+@pytest.mark.skipif(platform.system() == "Windows")
 def test_host_system(host):
     '''
     Check OS properties of the host system.
@@ -15,6 +17,17 @@ def test_host_system(host):
     assert distribution == host.system_info.distribution
     assert release == host.system_info.release
 
+
+@pytest.mark.skipif(platform.system() == "Linux")
+def test_host_system(host):
+    '''
+    Check OS properties of the host system.
+    '''
+    system_type = 'windows'
+    assert system_type == host.system_info.type
+
+
+@pytest.mark.skipif(platform.system() == "Windows")
 def test_app_installed(host):
     assert host.file("/opt/app/requirements.txt").exists
     assert host.file("/opt/app/main.py").exists
@@ -26,7 +39,6 @@ def test_service_packages(host):
     '''
     assert host.package("python").is_installed
     assert host.package("python-pip").is_installed
-    assert host.package("vim").is_installed
 
 
 def test_host_services(host):
